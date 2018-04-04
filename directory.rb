ae10605140bd1977c9ff4fe539570be92a503d5f
Ellie Keen <elliekeen95@gmail.com>
2018-04-04 16:03:10 +0100

    Only prints students name that begin with 'D'

--- directory.rb
+++ directory.rb
@@ -1,19 +1,16 @@
 def input_students
     puts "Please enter the names of the students"
     puts "To finish, just hit return twice"
-    # create an empty array
+
     students = []
-    #get the first name
     name = gets.chomp
-    #while the name is not empty, repeat this code
+
     while !name.empty? do
-      #add student hash to the array
       students << {name: name, cohort: :november}
       puts "Now we have #{students.count} students"
-      #get another name from the user
       name = gets.chomp
     end
-#return the array of input_students
+
 students
 end
 
@@ -23,8 +20,10 @@ def print_header
 end
 
 def print(students)
-    students.each_with_index do |student, index|
-        puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    students.each do |student|
+      if student[:name].start_with?("D")
+        puts "#{student[:name]} (#{student[:cohort]} cohort)"
+      end
     end
 end
 
