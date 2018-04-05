8d685d8ff67592e09809f208d44f76b2c563bd66
Ellie Keen <elliekeen95@gmail.com>
2018-04-05 15:03:21 +0100

    Refactored interactive menu

--- directory.rb
+++ directory.rb
@@ -1,50 +1,52 @@
-def interactive_menu
-  students = []
-    loop do
-
-# 1. print the menu and ask the user what to do
-     puts "1. Input the students"
-     puts "2. Show the students"
-     puts "9. Exit"
-# 2. read the input and save it into a variable
-     selection = gets.chomp
-# 3. do what the user has asked.
-     case selection
-     when "1"
-        students = input_students
-# input the students
-     when "2"
-        print_header
-        print(students)
-        print_footer(students)
-# show the students
-     when "9"
-       exit
-     else puts "I don't know what you meant, try again"
-# 4. repeat from step 1
-     end
-   end
-end
-
+@students = []
 
 def input_students
     puts "Please enter the names of the students"
     puts "To finish, just hit return twice"
 
-    students = []
     name = gets.strip
 
     while !name.empty? do
-      students << {name: name, cohort: :november}
-      if students.count > 1
-          puts "Now we have #{students.count} students"
+      @students << {name: name, cohort: :november}
+      if @students.count > 1
+          puts "Now we have #{@students.count} students"
       else
-          puts "Now we have #{students.count} student"
+          puts "Now we have #{@students.count} student"
       end
           name = gets.strip
     end
+end
 
-students
+def interactive_menu
+    loop do
+      print_menu
+      process(gets.chomp)
+    end
+end
+
+def print_menu
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+end
+
+def show_students
+    print_header
+    print_students_list(@students)
+    print_footer(@students)
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you mean, try again"
+  end
 end
 
 def print_header
@@ -52,7 +54,7 @@ def print_header
     puts "------------------".center(40)
 end
 
-def print(students)
+def print_students_list(students)
 
     students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)".center(40)
@@ -63,7 +65,7 @@ def print_footer(students)
 
     puts "------------------".center(40)
 
-    if students.count > 1
+    if @students.count > 1
         puts "Overall, we have #{students.count} great students".center(40)
     else
         puts "Overall, we have #{students.count} great student".center(40)
