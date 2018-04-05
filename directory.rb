d429c2d562a4db24ccfa642c0c82b622380d2e1a
Ellie Keen <elliekeen95@gmail.com>
2018-04-05 17:13:44 +0100

    Added load students method

--- directory.rb
+++ directory.rb
@@ -1,30 +1,11 @@
 @students = []
 
-def save_students
-  file = File.open("students.csv", "w")
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
-  end
-  file.close
-end
-
-def input_students
-    puts "Please enter the names of the students"
-    puts "To finish, just hit return twice"
-
-    name = gets.strip
-
-    while !name.empty? do
-      @students << {name: name, cohort: :november}
-      if @students.count > 1
-          puts "Now we have #{@students.count} students"
-      else
-          puts "Now we have #{@students.count} student"
-      end
-          name = gets.strip
-    end
+def print_menu
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "3. Save the list to students.csv"
+    puts "4. Load the list from students.csv"
+    puts "9. Exit"
 end
 
 def interactive_menu
@@ -32,19 +13,6 @@ def interactive_menu
       print_menu
       process(gets.chomp)
     end
-end
-
-def print_menu
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "3. Save the list to students.csv"
-    puts "9. Exit"
-end
-
-def show_students
-    print_header
-    print_students_list(@students)
-    print_footer(@students)
 end
 
 def process(selection)
@@ -55,11 +23,36 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit
   else
     puts "I don't know what you mean, try again"
   end
+end
+
+def input_students
+    puts "Please enter the names of the students"
+    puts "To finish, just hit return twice"
+
+    name = STDIN.gets.strip
+
+    while !name.empty? do
+      @students << {name: name, cohort: :november}
+      if @students.count > 1
+          puts "Now we have #{@students.count} students"
+      else
+          puts "Now we have #{@students.count} student"
+      end
+          name = STDIN.gets.strip
+    end
+end
+
+def show_students
+    print_header
+    print_students_list(@students)
+    print_footer(@students)
 end
 
 def print_header
@@ -83,6 +76,25 @@ def print_footer(students)
     else
         puts "Overall, we have #{students.count} great student".center(40)
     end
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
 end
 
 interactive_menu
