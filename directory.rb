a6365c90e3be201f3b0762d569a990e386fe4d7a
Ellie Keen <elliekeen95@gmail.com>
2018-04-08 16:47:14 +0100

    Add 'add_student' method

--- directory.rb
+++ directory.rb
@@ -16,20 +16,20 @@ def interactive_menu
 end
 
 def process(selection)
-  case selection
-  when "1"
-    input_students
-  when "2"
-    show_students(@students)
-  when "3"
-    save_students
-  when "4"
-    load_students
-  when "9"
-    exit
-  else
-    puts "I don't know what you mean, try again"
-  end
+    case selection
+    when "1"
+        input_students
+    when "2"
+        show_students(@students)
+    when "3"
+        save_students
+    when "4"
+        load_students
+    when "9"
+        exit
+    else
+        puts "I don't know what you mean, try again"
+    end
 end
 
 def input_students
@@ -39,14 +39,18 @@ def input_students
     name = STDIN.gets.chomp
 
     while !name.empty? do
-      @students << {name: name, cohort: :november}
-      if @students.count > 1
-          puts "Now we have #{@students.count} students"
-      else
-          puts "Now we have #{@students.count} student"
-      end
-          name = STDIN.gets.chomp
-    end
+        add_student(name)
+        if @students.count > 1
+            puts "Now we have #{@students.count} students"
+        else
+            puts "Now we have #{@students.count} student"
+        end
+            name = STDIN.gets.chomp
+        end
+end
+
+def add_student(name, cohort=:november)
+    @students << {name: name, cohort: cohort.to_sym}
 end
 
 def show_students(students)
@@ -61,16 +65,13 @@ def print_header
 end
 
 def print_student_list
-
     @students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)".center(40)
+        puts "#{student[:name]} (#{student[:cohort]} cohort)".center(40)
     end
 end
 
 def print_footer
-
     puts "------------------".center(40)
-
     if @students.count > 1
         puts "Overall, we have #{@students.count} great students".center(40)
     else
@@ -79,34 +80,34 @@ def print_footer
 end
 
 def save_students
-  file = File.open("students.csv", "w")
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
-  end
-  file.close
+    file = File.open("students.csv", "w")
+    @students.each do |student|
+        student_data = [student[:name], student[:cohort]]
+        csv_line = student_data.join(",")
+        file.puts csv_line
+    end
+    file.close
 end
 
 def load_students(filename = "students.csv")
-  file = File.open("students.csv", "r")
-  file.readlines.each do |line|
-  name, cohort = line.chomp.split(',')
-    @students << {name: name, cohort: cohort.to_sym}
-  end
-  file.close
+    file = File.open("students.csv", "r")
+    file.readlines.each do |line|
+        name, cohort = line.chomp.split(',')
+        add_student(name, cohort)
+    end
+    file.close
 end
 
 def try_load_students
-  filename = ARGV.first #first argument from the command line
-  return if filename.nil? #get out of the method if it isn't given
-  if File.exists?(filename) #if it exists
-    load_students(filename)
-    puts "Loaded #{@students.count} from #{filename}"
-  else #if it doesn't exist
-    puts "Sorry, #{filename} doesn't exist."
-    exit #quit the program
-  end
+    filename = ARGV.first
+    return if filename.nil?
+    if File.exists?(filename)
+        load_students(filename)
+        puts "Loaded #{@students.count} from #{filename}"
+    else
+        puts "Sorry, #{filename} doesn't exist."
+        exit
+    end
 end
 
 try_load_students
